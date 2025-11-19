96f6f48e83552206413832f9c099ad405988527f
Mingfei Wang <Mingfei.Wang@kpmg.co.uk>
2025-11-19 16:39:19 +0000

    federated_identity

--- aad_oidc.tf
+++ aad_oidc.tf
@@ -3,13 +3,14 @@ data "azuread_application" "tti_mingfei_poc" {
 }
 
 data "azuread_service_principal" "tti_mingfei_poc" {
-  display_name = "tti-mingfei-poc"
+  application_id = data.azuread_application.tti_mingfei_poc.client_id
 }
 
-resource "azuread_application_federated_identity_credential" "gh_fed" {
-  application_id = "/applications/${data.azuread_application.tti_mingfei_poc.object_id}"
-  display_name   = "github-actions-oidc"
-  issuer         = "https://token.actions.githubusercontent.com"
-  subject        = "repo:${var.github_repo_owner}/${var.github_repo_name}:ref:refs/heads/${var.github_branch}"
+resource "azuread_application_federated_identity_credential" "github" {
+  application_id = data.azuread_application.tti_mingfei_poc.id
+  display_name   = "github-actions"
+  description    = "OIDC for GitHub Actions"
   audiences      = ["api://AzureADTokenExchange"]
+  issuer         = "https://token.actions.githubusercontent.com"
+  subject        = "repo:mingfeiw/bookapi-minimal-infrastructure:ref:refs/heads/main"
 }
