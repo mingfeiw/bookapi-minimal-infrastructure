df3c581e705fe893c0c0e06ed4addc373396aa2a
Mingfei Wang <Mingfei.Wang@kpmg.co.uk>
2025-11-20 09:52:52 +0000

    bookapi-container

--- backend.tf
+++ backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "azurerm" {
     resource_group_name  = "rg-bookapi-minimal"
     storage_account_name = "stbookapiminimal"
-    container_name       = "acrbookapi"
+    container_name       = "bookapi-container"
     key                  = "terraform.tfstate"
   }
 }
