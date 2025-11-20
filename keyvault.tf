5304860e3fb8546d3dfa42ed1070e48264ff2342
Mingfei Wang <Mingfei.Wang@kpmg.co.uk>
2025-11-20 12:42:15 +0000

    purge true

--- keyvault.tf
+++ keyvault.tf
@@ -1,9 +1,10 @@
 resource "azurerm_key_vault" "bookapi_kv" {
-  name                = "kv-bookapiminimal"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  tenant_id           = data.azurerm_client_config.current.tenant_id
-  sku_name            = "standard"
+  name                     = "kv-bookapiminimal"
+  location                 = azurerm_resource_group.rg.location
+  resource_group_name      = azurerm_resource_group.rg.name
+  tenant_id                = data.azurerm_client_config.current.tenant_id
+  sku_name                 = "standard"
+  purge_protection_enabled = true
 
   access_policy {
     tenant_id          = data.azurerm_client_config.current.tenant_id
