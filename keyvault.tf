85e9472ad376e4f14fb8708eedc0fc7c31f5da97
Mingfei Wang <Mingfei.Wang@kpmg.co.uk>
2025-11-25 13:17:55 +0000

    access

--- keyvault.tf
+++ keyvault.tf
@@ -6,6 +6,14 @@ resource "azurerm_key_vault" "bookapi_kv" {
   sku_name                 = "standard"
   purge_protection_enabled = false
 
+  # Access policy for Terraform/GitHub Actions service principal
+  access_policy {
+    tenant_id          = data.azurerm_client_config.current.tenant_id
+    object_id          = data.azurerm_client_config.current.object_id
+    secret_permissions = ["Get", "List"]
+  }
+
+  # Access policy for AKS cluster
   access_policy {
     tenant_id          = data.azurerm_client_config.current.tenant_id
     object_id          = azurerm_kubernetes_cluster.aks.identity[0].principal_id
