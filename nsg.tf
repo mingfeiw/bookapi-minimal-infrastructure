654ea29b437c3b816a92a576bebeea4cdae10182
Mingfei Wang <Mingfei.Wang@kpmg.co.uk>
2025-11-25 10:39:26 +0000

    nsg

--- nsg.tf
+++ nsg.tf
@@ -1,18 +1,32 @@
 resource "azurerm_network_security_group" "nsg" {
-  name                = "your-nsg-name"
+  name                = "bookapi-nsg"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
 }
 
 resource "azurerm_network_security_rule" "allow_http" {
   name                        = "Allow-HTTP-80"
-  priority                    = 100
+  priority                    = 120
   direction                   = "Inbound"
   access                      = "Allow"
   protocol                    = "Tcp"
   source_port_range           = "*"
   destination_port_range      = "80"
   source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.rg.name
+  network_security_group_name = azurerm_network_security_group.nsg.name
+}
+
+resource "azurerm_network_security_rule" "allow_aks_api" {
+  name                        = "Allow-AKS-API"
+  priority                    = 110
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "Tcp"
+  source_port_range           = "*"
+  destination_port_range      = "443"
+  source_address_prefix       = "AzureCloud"
   destination_address_prefix  = "*"
   resource_group_name         = azurerm_resource_group.rg.name
   network_security_group_name = azurerm_network_security_group.nsg.name
