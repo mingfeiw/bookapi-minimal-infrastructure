b1e3f6a6ccffc5d3eb3fcdbabca08e5cf6750ecd
Mingfei Wang <Mingfei.Wang@kpmg.co.uk>
2025-11-25 11:34:51 +0000

    clean up

--- backend.tf
+++ backend.tf
@@ -1,9 +1,8 @@
 terraform {
-  # Temporarily commented out while recreating infrastructure
-  # backend "azurerm" {
-  #   resource_group_name  = "rg-bookapi-minimal"
-  #   storage_account_name = "stbookapiminimal"
-  #   container_name       = "bookapi-container"
-  #   key                  = "terraform.tfstate"
-  # }
+  backend "azurerm" {
+    resource_group_name  = "rg-bookapi-minimal"
+    storage_account_name = "stbookapiminimal"
+    container_name       = "bookapi-container"
+    key                  = "terraform.tfstate"
+  }
 }
