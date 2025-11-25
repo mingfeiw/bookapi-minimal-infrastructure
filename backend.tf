6a76f1680f292f089e47d07d4a0df60ab9df8437
Mingfei Wang <Mingfei.Wang@kpmg.co.uk>
2025-11-25 10:14:20 +0000

    remove qualys

--- backend.tf
+++ backend.tf
@@ -1,9 +1,8 @@
 terraform {
-  # Temporarily using local backend while recreating infrastructure
-  # backend "azurerm" {
-  #   resource_group_name  = "rg-bookapi-minimal"
-  #   storage_account_name = "stbookapiminimal"
-  #   container_name       = "bookapi-container"
-  #   key                  = "terraform.tfstate"
-  # }
+  backend "azurerm" {
+    resource_group_name  = "rg-bookapi-minimal"
+    storage_account_name = "stbookapiminimal"
+    container_name       = "bookapi-container"
+    key                  = "terraform.tfstate"
+  }
 }
